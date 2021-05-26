e439d4c1426716cb20d462a4dd9b7541720548d3
Nathanael White <narhanaelw99@gmail.com>
2021-05-26 15:23:52 -0500

    implemented the front end to accept post fetch requests

--- server.js
+++ server.js
@@ -1,36 +1,30 @@
+const fs = require("fs");
+const path = require("path");
 const express = require("express");
 const { animals } = require("./data/animals");
-const fs = require('fs');
-const path = require('path');
 
 const PORT = process.env.PORT || 3001;
-
 const app = express();
 
+app.use(express.static('public'));
 //parse incoming string or array
 app.use(express.urlencoded({ extended: true }));
 // perse incoming JSON data
 app.use(express.json());
+
 
 function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = [];
   let filteredResults = animalsArray;
 
   if (query.personalityTraits) {
-    // save personalityTraitsArray as a dedicated array
-    // if personalityTraitsArray is a string, place it into a new array and save.
     if (typeof query.personalityTraits === "string") {
       personalityTraitsArray = [query.personalityTraits];
     } else {
       personalityTraitsArray = query.personalityTraits;
     }
-    // loop through each trait in the personalityTraits array:
+
     personalityTraitsArray.forEach((trait) => {
-      // Check the trait against each animal in the filteredResults array.
-      // For each trait being targeted by the filter, the filteredResults
-      // array will then contain only the entries that contain the trait,
-      // so at the end we'll have an array of animals that have every one
-      // of the traits when the .forEach() loop is finished.
       filteredResults = filteredResults.filter(
         (animal) => animal.personalityTraits.indexOf(trait) !== -1
       );
@@ -64,21 +58,21 @@ function createNewAnimal(body, animalsArray) {
   const animal = body;
   animalsArray.push(animal);
   fs.writeFileSync(
-    path.join(__dirname, './data/animals.json'),
+    path.join(__dirname, "./data/animals.json"),
     JSON.stringify({ animals: animalsArray }, null, 2)
   );
   // return finished code to post route for response
   return animal;
-};
+}
 
 function validateAnimal(animal) {
-  if (!animal.name || typeof animal.name !== 'string') {
+  if (!animal.name || typeof animal.name !== "string") {
     return false;
   }
-  if (!animal.species || typeof animal.species !== 'string') {
+  if (!animal.species || typeof animal.species !== "string") {
     return false;
   }
-  if (!animal.diet || typeof animal.diet !== 'string') {
+  if (!animal.diet || typeof animal.diet !== "string") {
     return false;
   }
   if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
@@ -104,18 +98,34 @@ app.get("/api/animals/:id", (req, res) => {
   }
 });
 
-app.post('/api/animals', (req, res) => {
+app.post("/api/animals", (req, res) => {
   // set id based on what the next index of the array will be
   req.body.id = animals.length.toString();
 
   // if any data in req.body is incorrect, send 400 error back
   if (!validateAnimal(req.body)) {
-    res.status(400).send('The animal is not properly formatted.');
+    res.status(400).send("The animal is not properly formatted.");
   } else {
     // add animal to json file and animals array in this function
     const animal = createNewAnimal(req.body, animals);
-    res.json(req.body);
+    res.json(animal);
   }
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.listen(PORT, () => {
